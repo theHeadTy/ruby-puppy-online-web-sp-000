d374e804cb2306b814e99cacd20a11d5a03f2ba4
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-11 00:55:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,12 +4,12 @@ class Dog
   @@all = []
 
   def initialize(name)
-    @name = name
+    @name = self
     save()
   end
 
   def self.all
-    @@all.inspect
+    @@all
   end
 
   def self.clear_all
