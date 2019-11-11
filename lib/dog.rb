f4f86f6c9d2b98423d2cc068fea733ff7f0f36f8
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-11 01:23:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,45 @@
-# Add your code here
+class Dog
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.print_all
+    self.all.map { |n| puts n.name }.join("\n")
+  end
+
+  def save
+   self.class.all << self
+  end
+
+  def name
+    @name
+  end
+end
+
+# create dog instance
+dog = Dog.new('Snoopy')
+Dog.clear_all
+
+
+# create new dogs to add to @@all[]
+pluto = Dog.new('Pluto')
+fido = Dog.new('Fido')
+maddy = Dog.new('Maddy')
+
+# testing def save method
+Dog.clear_all
+pluto.save
+maddy.save
